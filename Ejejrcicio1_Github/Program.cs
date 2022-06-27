7074664d12c1948e8abad0552745b113aac69ef4
Nicolas Brosan <nicolasbrosan@hotmail.com>
2022-06-26 21:54:09 -0300

    primer commit cargado

---
+++ Ejejrcicio1_Github/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejejrcicio1_Github
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("##### Bienvenido!! ######");
+            Console.WriteLine("");
+            Console.WriteLine("Por favor, ingrese un numero: ");
+            List<int> numeros = new List<int>();
+            var num = Convert.ToInt32(Console.ReadLine());
+            var flag = true;
+            while(flag == true)
+            {
+                numeros.Add(num);
+                
+                Console.WriteLine("Desea agregar otro numero? Si(s) / No(n)");
+                var usuario = Console.ReadLine();
+                if (usuario == "s")
+                {
+                    flag = true;
+                }
+                else if(usuario == "n")
+                {
+                    flag = false;
+                    break;
+                }
+                Console.WriteLine("Por favor, ingrese un numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            var contador = numeros.Count();
+            var suma = numeros.Sum();
+            decimal prom = suma / contador;
+            var Max = numeros.Max();
+            var Min = numeros.Min();
+            Console.WriteLine("La suma es {0}", suma);
+            Console.WriteLine("El promedio es {0}", prom);
+            Console.WriteLine("El valor maximo es: {0} y el valor minimo es: {1}", Max, Min);
+
+            Console.ReadKey();
+        }
+    }
+}
